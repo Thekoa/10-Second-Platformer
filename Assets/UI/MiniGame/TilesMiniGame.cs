f372e7e5592edc1435b342804da85b6fbed98d05
Xavier <67317037+Thekoa@users.noreply.github.com>
2022-09-28 13:53:43 +1000

    Making a Game Over Menu

--- Assets/UI/MiniGame/TilesMiniGame.cs
+++ Assets/UI/MiniGame/TilesMiniGame.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class TilesMiniGame : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       
+       
+         //Assign numbers to each button
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
