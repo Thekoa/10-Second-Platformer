b72c8773f3792f599ddf3cf43d721631516bc4ec
lsellman16 <99856297+lsellman16@users.noreply.github.com>
2022-10-02 13:48:54 +1100

    Ran into issues with playback of Jump sound
    ArgumentNullException: Value cannot be null.
    seems to be a problem when the Jump is  pressed, fix quickly

--- Assets/Scripts/Player/PlayerDeath.cs
+++ Assets/Scripts/Player/PlayerDeath.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class PlayerDeath : MonoBehaviour
 {
+    //[SerializeField] public AudioClip _deathSound;
     public GameObject gameOverMenu;
     //Death functions, pauses when the player touches the deathray. 
     private void OnTriggerEnter2D (Collider2D other)
     {
         if(other.tag=="Player")
         {
+            //SoundManager.Instance.PlaySound(_deathSound);
             Time.timeScale = 0f;
             gameOverMenu.SetActive(true);
         }
