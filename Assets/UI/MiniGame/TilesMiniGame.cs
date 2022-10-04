2fa8edbd6f24a7d3f1243256b65f16285bec6fd8
Xavier <67317037+Thekoa@users.noreply.github.com>
2022-10-04 16:28:58 +1100

    importing changes

--- Assets/UI/MiniGame/TilesMiniGame.cs
+++ Assets/UI/MiniGame/TilesMiniGame.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class TilesMiniGame : MonoBehaviour
 {
+    int randomNumber;
+    int lastNumber;
+    int maxAttempts = 10;
+
+    void NewRandomNumber()
+    {
+        for (int i = 0; randomNumber == lastNumber && i < maxAttempts; i++)
+        {
+            randomNumber = Random.Range(0, 10);
+        }
+        lastNumber = randomNumber;
+    }
 
     // Start is called before the first frame update
     void Start()
